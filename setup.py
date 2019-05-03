cf67282ab00609149c33ba4b935b2a61f6f2f2b2
Andreas Schroeder <andreas@a-netz.de>
2019-05-03 10:33:48 +0800

    Added initial setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Distutils',
+    version='0.0-dev1',
+    description='PyBinMap interprets binary data',
+    author='Andreas Schroeder',
+    author_email='andreas@a-netz.de',
+    url='https://github.com/motlib/pybinmap',
+    packages=['pybinmap'],
+)
