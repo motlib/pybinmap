35598474378688ae55b6b10f3bd778c44d4a86e6
Andreas Schroeder <andreas@a-netz.de>
2019-05-03 10:49:54 +0800

    Extended tests.

--- pybinmap/test/test_binmap.py
+++ pybinmap/test/test_binmap.py
@@ -3,7 +3,7 @@ import pytest
 from .. import BinMap
 
 
-testdata = bytes([0x12, 0x34, 0x56, 0x78, 0x34, 0x32])
+testdata = bytes([0x12, 0x34, 0x56, 0x78, 0x34, 0x32, 0x30, 0x30, 0x20])
 
 
 def test_binmap():
@@ -48,3 +48,31 @@ def test_binmap_str():
     bm.set_data(testdata)
     
     print(bm)
+
+testspec_all_datatypes = [
+    ({'dt':'raw', 'start':0, 'length':8}, bytes([0x12])),
+    ({'dt':'uint', 'start':0, 'length':8}, 0x12),
+    ({'dt':'uint8', 'start':0}, 0x12),
+    ({'dt':'uint16', 'start':0}, 0x3412),
+    ({'dt':'uint16', 'start':0, 'endian': 'big'}, 0x1234),
+    ({'dt':'uint32', 'start':0}, 0x78563412),
+    ({'dt':'uint64', 'start':0}, 0x3030323478563412),
+    ({'dt':'ascii', 'start':4*8, 'length':16}, '42'),
+    ({'dt':'utf8', 'start':4*8, 'length':16}, '42'),
+    ({'dt':'bool', 'start':0, 'length':1}, False),
+    ({'dt':'bool1', 'start':1}, True),
+    ({'dt':'bool8', 'start':0}, True),
+]
+
+
+
+@pytest.mark.parametrize('params,result', testspec_all_datatypes)    
+def test_all_datatypes(params, result):
+    bm = BinMap()
+
+    params['name'] = 'testval'
+    
+    bm.add(**params)
+    bm.set_data(testdata)
+
+    assert bm.get_value('testval') == result
