15c3a95ef4fe3b8a1f38f058040e87a991136380
Andreas Schroeder <andreas@a-netz.de>
2019-05-03 10:25:46 +0800

    Created readme. Updated test code to reflect code in readme.

---
+++ README.md
@@ -0,0 +1,50 @@
+# PyBinMap
+
+`pybinmap` is a library to help interpreting binary data. This is e.g. useful
+when reverse engineering a data structure or when receiving any kind of
+unstructured binary data dump and need to make sense of it. 
+
+The functionality is comparable to the `struct` module in the Python standard
+library, but with many more features. 
+
+
+## Usage
+
+Let's say you have binary data like this:
+
+```python
+data = bytes([0x12, 0x34, 0x56, 0x78, 0x34, 0x32])
+```
+
+And you know, that there is 
+
+* a bit called `enabled` in the first byte, bit 2
+* an 4 bit unsigned int called `testval` in the second byte
+* an two-character ascii string called `answer` in bytes 4 and 5
+
+Then you can do the following:
+
+```python
+bm = BinMap()
+bm.add(dt='bool', name='enabled', start=2, length=1)
+bm.add(dt='uint', name='testval', start=8, length=4)
+bm.add(dt='ascii', name=answer', start=4*8, length=2*8)
+
+print(str(bm))
+```
+
+The result looks like this:
+
+```text
+0000:1+1 enabled = True [raw: 0x01]
+0001:0+8 testval = 52 [raw: 0x34]
+0003:0+16 answer = x4 [raw: 0x78 0x34]
+```
+
+Or you can retrieve single values or a dictionary with all values:
+
+```python
+answer = bm.get_value('answer')
+idata = bm.get_dict()
+```
+
--- pybinmap/test/test_binmap.py
+++ pybinmap/test/test_binmap.py
@@ -3,26 +3,27 @@ import pytest
 from .. import BinMap
 
 
-testdata = bytes([0x12, 0x34, 0x56, 0x78, 0x90, 0x31, 0x34, 0x34])
+testdata = bytes([0x12, 0x34, 0x56, 0x78, 0x34, 0x32])
 
 
 def test_binmap():
     bm = BinMap()
-    bm.add(dt='bool', name='b1', start=1, length=1)
-    bm.add(dt='ascii', name='ascii', start=8*5, length=8*3)
-    bm.add(dt='uint', name='test', start=0, length=8)
+    
+    bm.add(dt='bool', name='enabled', start=1, length=1)
+    bm.add(dt='uint', name='testval', start=8, length=8)
+    bm.add(dt='ascii', name='answer', start=8*4, length=8*2)
 
     bm.set_data(testdata)
 
-    assert bm.get_value('b1') == 1
-    assert bm.get_value('ascii') == '144'
-    assert bm.get_value('test') == 0x12
+    assert bm.get_value('enabled') == 1
+    assert bm.get_value('testval') == 0x34
+    assert bm.get_value('answer') == '42'
 
     
 def test_binmap_predef_params():
     bm = BinMap()
     bm.add(dt='bool1', name='b1', start=1)
-    bm.add(dt='ascii', name='ascii', start=8*5, length=8*3)
+    bm.add(dt='ascii', name='ascii', start=8*3, length=8*3)
     bm.add(dt='uint', name='test', start=0, length=8)
 
     bm.set_data(testdata)
@@ -39,9 +40,10 @@ def test_binmap_missing_param():
         
 def test_binmap_str():
     bm = BinMap()
-    bm.add(dt='bool', name='b1', start=1, length=1)
-    bm.add(dt='ascii', name='ascii', start=8*5, length=8*3)
-    bm.add(dt='uint', name='test', start=0, length=8)
+    
+    bm.add(dt='bool', name='enabled', start=1, length=1)
+    bm.add(dt='uint', name='testval', start=8, length=8)
+    bm.add(dt='ascii', name='answer', start=8*3, length=8*2)
 
     bm.set_data(testdata)
     
