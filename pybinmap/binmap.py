b92f410e5972710ebb6bd3a9f2a1ea92ed63c5f8
Andreas Schroeder <andreas@a-netz.de>
2019-05-11 12:17:37 +0800

    Install pylint. Remove trailing whitespaces.

--- pybinmap/binmap.py
+++ pybinmap/binmap.py
@@ -5,13 +5,13 @@ from .dataitems import *
 class BinMap():
     '''The BinMap organized a set of data items to interpret unstructured binary
     data.'''
-    
+
     def __init__(self):
         # a list for maintaining sort order of the items
         self._map_list = []
         # a dict for name based access
         self._map_dict = {}
-        
+
         self._data = None
 
         self._type_tbl = {
@@ -30,69 +30,69 @@ class BinMap():
 
         self._unmapped_counter = 0
 
-        
+
     def add(self, **kwargs):
         '''Add a new data item definition to this binary map.'''
 
         if 'dt' not in kwargs:
             raise ValueError("Must specify the data type with 'dt' parameter.")
-        
+
         spec = self._type_tbl[kwargs['dt']]
 
         kwargs.update(spec)
-        
+
         # we do not pass on the class
         del kwargs['cls']
-        
+
         cls = spec['cls']
         bd = cls(**kwargs)
 
         self._add_bd(bd)
 
-        
+
     def add_from_spec(self, spec):
         '''Use a list of dict structure to specify data items to add.'''
-        
+
         for item in spec:
             self.add(**spec)
 
-            
+
     def get_spec(self):
         return [item.spec for item in self._map_list]
 
-    
+
     def _add_bd(self, bd):
         '''Add a DataItem instance.'''
-        
+
         self._map_dict[bd.name] = bd
         self._map_list.append(bd)
-        
+
         # keep the list sorted by start address
         self._map_list.sort(key=lambda bd: bd.start)
 
-        
+
     def set_data(self, data):
         '''Set the data to be interpreted.
 
         :param data: A bytes() object containing the data to be interpreted.'''
-        
+
         for bd in self._map_list:
             bd.set_data(data)
 
-            
+
     def get_value_dict(self):
         '''Return an ordered dictionary containing mapping the data item names to their
         values. The dict is ordered by start address of the data items.
 
         '''
-        
+
         odict = OrderedDict()
         for bd in self._map_list:
             odict[bd.get_name()] = bd.get_value()
 
         return odict
 
-    
+
     def get_value(self, name):
         '''Return the value of a data item by its name.'''
 
@@ -101,18 +101,18 @@ class BinMap():
 
     def __getitem__(self, key):
         '''Index based access to values.'''
-        
+
         return self._map_dict[key].value
-    
-    
+
+
     def get_item(self, name):
         '''Returns the underlying DataItem instance for the given name.'''
         return self._map_dict[name]
-    
+
 
     def _add_unmapped(self, start, end):
         '''Add a 'raw' data item for the given address range.'''
-        
+
         self.add(
             dt='raw',
             name='unmapped_{:03}'.format(self._unmapped_counter),
@@ -120,10 +120,10 @@ class BinMap():
             length=(end - start + 1))
 
         self._unmapped_counter += 1
-                
+
 
     def fill_unmapped(self, end_addr=None):
-        '''Fill all unmapped areas 'raw' type mappings. 
+        '''Fill all unmapped areas 'raw' type mappings.
 
         If end_addr is not specified, address space is filled up to the last
         mapped data item. Adress space beyond the last data item is not filled
@@ -144,9 +144,9 @@ class BinMap():
         if (end_addr is not None) and (end_addr > self._map_list[-1].end):
             self._add_unmapped(self._map_list[-1].end + 1, end_addr)
 
-                
+
     def __str__(self):
         '''Convert the whole binmap to string, i.e. print the information from all
         contained data items.'''
-        
+
         return '\n'.join(str(bd) for bd in self._map_list)
--- pybinmap/test/test_binmap.py
+++ pybinmap/test/test_binmap.py
@@ -8,7 +8,7 @@ testdata = bytes([0x12, 0x34, 0x56, 0x78, 0x34, 0x32, 0x30, 0x30, 0x20])
 
 def _get_default_binmap():
     bm = BinMap()
-    
+
     bm.add(dt='bool', name='enabled', start=1, length=1)
     bm.add(dt='uint', name='testval', start=8, length=8)
     bm.add(dt='ascii', name='answer', start=8*4, length=8*2)
@@ -20,26 +20,26 @@ def _get_default_binmap():
 
 def test_binmap():
     bm = _get_default_binmap()
-    
+
     assert bm.get_value('enabled') == 1
     assert bm.get_value('testval') == 0x34
     assert bm.get_value('answer') == '42'
 
-    
+
 def test_binmap_setup_missing_param():
     bm = _get_default_binmap()
 
     with pytest.raises(KeyError):
         bm.add(dt='bool1', name='b1')
 
-        
+
 def test_binmap_str():
     bm = _get_default_binmap()
 
     # ensure that we can convert to string
     print(bm)
 
-    
+
 testspec_all_datatypes = [
     ({'dt':'raw', 'start':0, 'length':8}, bytes([0x12])),
     ({'dt':'uint', 'start':0, 'length':8}, 0x12),
@@ -55,14 +55,14 @@ testspec_all_datatypes = [
     ({'dt':'bool8', 'start':0}, True),
 ]
 
-@pytest.mark.parametrize('params,result', testspec_all_datatypes)    
+@pytest.mark.parametrize('params,result', testspec_all_datatypes)
 def test_all_datatypes(params, result):
     '''Run through all available data types and use them at least once.'''
-    
+
     bm = BinMap()
 
     params['name'] = 'testval'
-    
+
     bm.add(**params)
     bm.set_data(testdata)
 
@@ -79,7 +79,7 @@ def test_fill_unmapped():
 
     bm = _get_default_binmap()
     bm.fill_unmapped()
-    
+
     bm.set_data(testdata)
 
     um0 = bm.get_item('unmapped_000')
@@ -100,7 +100,7 @@ def test_fill_unmapped():
 
 def test_fill_unmapped_with_end_address():
     '''Check if filling up to end address works.'''
-    
+
     bm = _get_default_binmap()
     bm.fill_unmapped(end_addr=800)
 
@@ -108,7 +108,7 @@ def test_fill_unmapped_with_end_address():
     assert um3.start == 48
     assert um3.end == 800
 
-    
+
 def test_dict_access():
     '''Test successful dict based access to binmap information.'''
 
@@ -117,11 +117,11 @@ def test_dict_access():
     assert bm['enabled'] == True
     assert bm['testval'] == 0x34
 
-    
+
 def test_dict_access_non_existent_element():
     '''Test unsuccessful dict based access to binmap information.'''
 
     bm = _get_default_binmap()
-    
+
     with pytest.raises(KeyError):
         bm['non_existent_value']
