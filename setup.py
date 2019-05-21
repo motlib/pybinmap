3e6db97b03db62d5cba62019d841a57fb540934d
Andreas Schroeder <andreas@a-netz.de>
2019-05-21 13:48:39 +0800

    Fix setup.py (use setuptools). Install wheel dependency. Add build directories to gitignore.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pybinmap',
     version='0.0-dev1',
     description='PyBinMap helps to interpret binary data blobs.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Andreas Schroeder',
     author_email='andreas@a-netz.de',
     url='https://github.com/motlib/pybinmap',
